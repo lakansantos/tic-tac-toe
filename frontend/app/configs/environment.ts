5d50ff072098cc5a222907372b27dcd1475155ca
lakansantos <rizesuoracle25@gmail.com>
2024-06-08 14:53:46 +0800

    Added production adjustment for API

--- frontend/app/configs/environment.ts
+++ frontend/app/configs/environment.ts
@@ -1,1 +1,7 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const ENV_ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT;
+export const API_URL_LOCAL = process.env.NEXT_PUBLIC_API_URL_LOCAL;
+export const API_URL_PRODUCTION = process.env.NEXT_PUBLIC_API_URL_PRODUCTION;
+export const BASE_URL =
+  ENV_ENVIRONMENT === 'production' ? API_URL_PRODUCTION : API_URL_LOCAL;
+
+console.log(BASE_URL);
